3989eaada51b59298132d90b2bf77942973550b4
KennedyUC <chukwukakennedyuche@gmail.com>
2021-12-09 09:31:03 -0800

    providers.tf updated

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53, < 5.0"
+    }
+  }
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
