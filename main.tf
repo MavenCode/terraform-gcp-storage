57d905a1b2726e632cea3d75e3a69845acf75d16
KennedyUC <chukwukakennedyuche@gmail.com>
2021-12-09 17:40:18 -0800

    config file updated

--- main.tf
+++ main.tf
@@ -4,7 +4,10 @@ resource "google_storage_bucket" "gcp_bucket" {
   location      = var.gcs_region
   force_destroy = var.force_destroy
   storage_class = var.storage-class
-  versioning    = var.enable_versioning
+  
+  versioning {
+    enabled = var.enable_versioning
+  }
 }
 
 resource "google_storage_bucket_iam_binding" "storage_admin" {
